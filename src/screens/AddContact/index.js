8a73e1b8f272a232708b28e181c5dbd0cb402a0a
Nicolas Ocampo <nicolasocampo288@gmail.com>
2022-10-03 10:02:59 -0400

    10/3/2022

---
+++ src/screens/AddContact/index.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import styles from './styles';
+
+const database = require('../../components/Handlers/database.js');
+const AddContact = props => {
+
+        const navigation = useNavigation();
+        
+        const [fullname, setFullName] = useState('');
+        const [phone, setPhone] = useState('');
+        const [email, setEmail] = useState('');
+        
+
+        const onContactAdd = ()  => { //Validation
+                if(!fullname) {
+                        alert( 'Please enter a contact fullname.');
+                        return;
+                }
+                if(!phone ) {
+                        alert('Please enter a contact phone in format NPA-NXX-XXXX');
+                        return;
+                }
+                if(!email) {
+                        alert('Please enter contact email.');
+                        return;
+                } 
+
+                try {
+                        database.addContact(fullname, phone, email)
+                } catch (error) {
+                        console.log('Error adding contact  ' + error);
+                }
+        
+                
+                alert(fullname + ' Added ');
+                
+        }
+
+  return (
+    <View style={styles.container}>
+        <View style={styles.topContainer}>
+                <TextInput
+                        value={fullname}
+                        onChangeText={value => setFullName(value)}
+                        style={styles.fullname}
+                        clearButtonMode={'while-editing'}
+                        placeholder={'Enter Contact Name'}
+                        placeholderTextColor={'grey'}
+                />
+                <TextInput  
+                        value={phone}
+                        onChangeText={value => setPhone(value)}
+                        style={styles.phone}
+                        clearButtonMode={'while-editing'}
+                        placeholder={'Enter Phone in format NPA-NXX-XXXX'}
+                        placeholderTextColor={'grey'}
+                />
+                <TextInput  
+                        value={email}
+                        onChangeText={value => setEmail(value)}
+                        style={styles.email}
+                        clearButtonMode={'while-editing'}
+                        placeholder={'Enter Email'}
+                        placeholderTextColor={'grey'}
+                        
+                />
+
+        </View>
+        <View style={styles.bottomContainer}>
+                <Pressable style={styles.button} onPress={onContactAdd}>
+                        <Text style={styles.buttonText}> Add</Text>
+                </Pressable>
+
+        </View>
+    </View>
+  );
+  };
+
+export default AddContact;
